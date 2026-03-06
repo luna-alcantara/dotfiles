f3fd40ab37c8c6ca90bb5582f3fc9b7d00181c8d
Luna <lunaarsilva@gmail.com>
2026-03-05 23:25:22 -0300

    add dap to nvim

---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- Debug Framework
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
+    -- config = function()
+    -- end,
+    event = 'VeryLazy',
+  },
+}
