3e32a34dda98d95d3cd255147c435ff533e6f8db
Luna <lunaarsilva@gmail.com>
2026-03-05 23:25:26 -0300

    update nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,18 +1,143 @@
 return {
   {
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
     dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
     lazy = false,
-    config = function()
-      require('oil').setup({
-        view_options = {
-          show_hidden = true,
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        'icon',
+      },
+      buf_options = {
+        buflisted = false,
+        bufhidden = 'hide',
+      },
+      win_options = {
+        wrap = false,
+        signcolumn = 'no',
+        cursorcolumn = false,
+        foldcolumn = '0',
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = 'nvic',
+      },
+      delete_to_trash = false,
+      skip_confirm_for_simple_edits = false,
+      prompt_save_on_select_new_entry = true,
+      cleanup_delay_ms = 2000,
+      lsp_file_methods = {
+        timeout_ms = 1000,
+        autosave_changes = false,
+      },
+      constrain_cursor = 'editable',
+      watch_for_changes = false,
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+        ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+        ['g\\'] = 'actions.toggle_trash',
+      },
+      use_default_keymaps = true,
+      view_options = {
+        show_hidden = false,
+        is_hidden_file = function(name)
+          return vim.startswith(name, '.')
+        end,
+        is_always_hidden = function()
+          return false
+        end,
+        natural_order = true,
+        case_insensitive = false,
+        sort = {
+          { 'type', 'asc' },
+          { 'name', 'asc' },
         },
-      })
+      },
+      extra_scp_args = {},
+      git = {
+        add = function()
+          return false
+        end,
+        mv = function()
+          return false
+        end,
+        rm = function()
+          return false
+        end,
+      },
+      float = {
+        padding = 2,
+        max_width = 0,
+        max_height = 0,
+        border = 'rounded',
+        win_options = {
+          winblend = 0,
+        },
+        preview_split = 'auto',
+        override = function(conf)
+          return conf
+        end,
+      },
+      preview = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = 0.9,
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = 'rounded',
+        win_options = {
+          winblend = 0,
+        },
+        update_on_cursor_moved = true,
+      },
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = 'rounded',
+        minimized_border = 'none',
+        win_options = {
+          winblend = 0,
+        },
+      },
+      ssh = {
+        border = 'rounded',
+      },
+      keymaps_help = {
+        border = 'rounded',
+      },
+    },
+    config = function(_, opts)
+      local oil = require('oil')
+      oil.setup(opts)
 
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open Oil' })
+      vim.keymap.set('n', '<A-a>', oil.open_float, { desc = 'Oil (float)' })
+
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function()
+          if vim.fn.argc() == 0 then
+            vim.cmd('Oil')
+          end
+        end,
+      })
     end,
   },
 }
