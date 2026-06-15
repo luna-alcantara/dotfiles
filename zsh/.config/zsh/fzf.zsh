6addcb4eac37dfb29a88c6284cf295e85f916a96
Luna <lunaarsilva@gmail.com>
2026-06-15 12:52:25 -0300

    update

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -40,7 +40,7 @@ _bookmark_fzf() {
   )
   if [[ -n "$result" ]]; then
     local url="${result##* | }"
-    (    (xdg-open "$url" &>/dev/null &) &>/dev/null &)
+    ("${BROWSER:-xdg-open}" "$url" &>/dev/null &) &>/dev/null &
   fi
   zle reset-prompt
 }
